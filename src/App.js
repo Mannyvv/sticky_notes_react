8c215b5cc8c35fafdaaadcc8ceadb88356234d4e
Manny Vera <vallesvera.manuel@gmail.com>
2024-01-02 04:40:52 -0800

    Functionality for adding,deleting,editing notes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState } from 'react';
+import { dummyData } from './dummyData';
 
-function App() {
+const App = () => {
+  const [notes, setNotes] = useState(dummyData);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [selectedNote, setSelectedNote] = useState(null)
+
+  const handleAddNote = (event) => {
+    event.preventDefault();
+    console.log("Title: ", title)
+    console.log("Content: ", content)
+
+    const newNote = {
+      id: notes.length + 1,
+      title: title,
+      content: content
+    }
+
+    setNotes([newNote, ...notes])
+    setTitle("")
+    setContent("")
+  }
+
+  const handleNoteclick = (note) => {
+    setSelectedNote(note)
+    setTitle(note.title)
+    setContent(note.content)
+  }
+
+  const handleUpdateNote = (event) => {
+    event.preventDefault();
+    if (!selectedNote) {
+      return;
+    }
+
+    const updatedNote = {
+      id: selectedNote.id,
+      title: title,
+      content: content
+    }
+
+    // Map through each item and will return updated note for matching id, only
+    // will return same note for all others
+    const updateNoteList = notes.map((note) =>
+      note.id === selectedNote.id ? updatedNote : note
+    )
+    setNotes(updateNoteList);
+    setSelectedNote(null);
+    setTitle("")
+    setContent("")
+  }
+
+  const handleCancel = () => {
+    setTitle("")
+    setContent("")
+    setSelectedNote(null)
+  }
+
+  const deleteNote = (event,note) => {
+    event.stopPropagation();
+    const selectedNote = note
+    const updateNoteList = notes.filter((note) => note.id != selectedNote.id)
+      setNotes(updateNoteList);
+      setSelectedNote(null);
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-container'>
+      <form className='note-form' onSubmit={selectedNote ? handleUpdateNote : handleAddNote}>
+        <input placeholder='Title' value={title} onChange={(event) => setTitle(event.target.value)} required />
+        <textarea placeholder='Content' rows={10} value={content} onChange={(event) => setContent(event.target.value)} required />
+        {selectedNote ? (
+          <div className='edit-buttons'>
+            <button type='submit'>Save</button>
+            <button onClick={handleCancel}>Cancel</button>
+
+          </div>) : (<button type='submit' >Add Note</button>)}
+      </form>
+
+      <div className='notes-grid'>
+        {notes.map((note) => {
+          return (
+            <div className='note-item' onClick={() => handleNoteclick(note)}  key={note.id}>
+              <div className='notes-header'>
+                <button onClick={(event) => deleteNote(event,note)}>x</button>
+              </div>
+              <h2>{note.title}</h2>
+              <p >{note.content}</p>
+            </div>
+          )
+        })}
+      </div>
     </div>
-  );
+  )
+
 }
 
 export default App;
---
+++ src/dummyData.js
@@ -0,0 +1,55 @@
+
+
+export const dummyData = [
+        {
+          id: 1,
+          title: "First Note",
+          content: "This is the content of the first note.",
+        },
+        {
+          id: 2,
+          title: "Second Note",
+          content: "Here goes the content of the second note.",
+        },
+        {
+          id: 3,
+          title: "Third Note",
+          content: "Content for third note can be added here.",
+        },
+        {
+          id: 4,
+          title: "Note Four",
+          content: "This is the content of the fourth note.",
+        },
+        {
+          id: 5,
+          title: "Fifth Note",
+          content: "Content for the fifth note.",
+        },
+        {
+          id: 6,
+          title: "Note Six",
+          content: "This is the content of the sixth note.",
+        },
+        {
+          id: 7,
+          title: "Seventh Note",
+          content: "Content for the seventh note.",
+        },
+        {
+          id: 8,
+          title: "Note Eight",
+          content: "This is the content of the eighth note.",
+        },
+        {
+          id: 9,
+          title: "Ninth Note",
+          content: "Content for the ninth note.",
+        },
+        {
+          id: 10,
+          title: "Tenth Note",
+          content: "This is the content of the tenth note.",
+        },
+      ];
+      
